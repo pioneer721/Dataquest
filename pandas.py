2f653342e0c15dd6d3902909d6ba46496b17bdaf
pwjeong14 <pilwon95@gmail.com>
2018-09-29 23:22:19 +0900

    Update pandas.py

--- pandas.py
+++ pandas.py
@@ -19,8 +19,8 @@ top_roa_by_sector= {}
 for sector in f500["sector"].unique():
     is_sector= f500["sector"]==sector					# BOOL 
     sector_companies= f500.loc[is_sector]				# selecting rows that satisfy bool	
-	top_company= sector_companies.sort_values("roa", ascending= False).iloc[0]	
-    #1- sort sector companies by "roa" descendingly. 
+    top_company= sector_companies.sort_values("roa", ascending= False).iloc[0]	
+    	#1- sort sector companies by "roa" descendingly. 
 	#2- iloc[0] -> the first of the slice, which is the highest roa
-	company_name= top_company.loc["company"] 			# company's name 					
-    top_roa_by_sector[sector]= company_name				# Adding name to dictionary 			
+    company_name= top_company.loc["company"] 			# company's name 					
+    top_roa_by_sector[sector]= company_name			# Adding name to dictionary 			
