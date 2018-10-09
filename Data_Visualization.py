aaed55381044d32a26f63ceeadcce034a486c3aa
pwjeong14 <pilwon95@gmail.com>
2018-10-09 13:54:41 +0900

    Update Data_Visualization.py

--- Data_Visualization.py
+++ Data_Visualization.py
@@ -1,4 +1,6 @@
-#BASICS 
+##### BASICS ######
+
+###Single plot
 import pandas as pd
 import matplotlib.pyplot as plt
 
@@ -12,5 +14,18 @@ plt.xticks(rotation=90)
 plt.xlabel("Month")
 plt.ylabel("Unemployment Rate")
 plt.title("Monthly Unemployment Trends, 1948")
+
+plt.show()
+
+### Multiple plots
+import matplotlib.pyplot as plt
+fig = plt.figure()
+
+ax1 = fig.add_subplot(2,1,1)    #Upper plot
+ax2 = fig.add_subplot(2,1,2)    #Bottom plot
+
+#plotting the subplots! 
+ax1.plot(unrate["DATE"][:12], unrate["VALUE"][:12])     # first 12 values 
+ax2.plot(unrate["DATE"][12:24], unrate["VALUE"][12:24]) # 12~24 values
 
 plt.show()
