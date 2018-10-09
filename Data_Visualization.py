87e2ba6a0ed75fcb9511ac0d8bdcc77d99cf794f
pwjeong14 <pilwon95@gmail.com>
2018-10-09 15:16:34 +0900

    Update Data_Visualization.py

--- Data_Visualization.py
+++ Data_Visualization.py
@@ -36,7 +36,7 @@ plt.show()
 
 ### Multiple lines in one plot
 
-#if theres only a few lines
+## if theres only a few lines
 unrate['DATE'] = pd.to_datetime(unrate['DATE'])   #converting the DATE column into a Series of datetime values.
 unrate['MONTH'] = unrate['DATE'].dt.month         #Extracting month values from "DATE" column
 
@@ -44,15 +44,19 @@ fig = plt.figure(figsize=(6,3))
 plt.plot(unrate[0:12]['MONTH'], unrate[0:12]['VALUE'], c='red')
 plt.plot(unrate[12:24]['MONTH'], unrate[12:24]['VALUE'], c='blue')
 
-#Smarter way (if there are a lot of lines) 
+## FULL, smarter way (if a lot of lines) 
 fig = plt.figure(figsize=(10,6))
 colors = ['red', 'blue', 'green', 'orange', 'black']
-labels= [1948, 1949, 1950, 1951, 1952]
-
 for i in range(5):
     start_index = i*12
     end_index = (i+1)*12
     subset = unrate[start_index:end_index]
-    plt.plot(subset['MONTH'], subset['VALUE'], c=colors[i], label= labels[i])
+    label = str(1948 + i)
+    plt.plot(subset['MONTH'], subset['VALUE'], c=colors[i], label=label)
+
+plt.title("Monthly Unemployment Trends, 1948- 1952")    
+plt.legend(loc='upper left') #a box of labels for line
+plt.xlabel("Month, Integer")
+plt.ylabel("Unemployment Rate, Percent")
 
 plt.show()
