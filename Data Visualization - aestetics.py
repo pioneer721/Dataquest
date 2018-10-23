a39a70e5ad34988f0e7a2057ad915b7c738ac697
pwjeong14 <pilwon95@gmail.com>
2018-10-23 22:24:43 +0900

    Create Data Visualization - aestetics.py

---
+++ Data Visualization - aestetics.py
@@ -0,0 +1,8 @@
+
+#plot of men and women percentage in bio degree over time 
+
+plt.plot(women_degrees['Year'], women_degrees['Biology'], c='blue', label='Women')
+plt.plot(women_degrees['Year'], 100-women_degrees['Biology'], c='green', label='Men')
+plt.legend(loc='upper right')
+plt.title('Percentage of Biology Degrees Awarded By Gender')
+plt.show()
