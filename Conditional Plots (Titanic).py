ea7a146a43a7ddc2ac6de6a125e05b7b1579d5b6
pwjeong14 <pilwon95@gmail.com>
2018-10-24 22:58:34 +0900

    Update Conditional Plots (Titanic).py

--- Conditional Plots (Titanic).py
+++ Conditional Plots (Titanic).py
@@ -1,5 +1,17 @@
 import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
 
 titanic= pd.read_csv("train.csv")
-cols= ["Survived", "Pclass", "Sex", "Age", "SibSp", "Parch", "Fare", "Embarked"]
-titanic= titanic[cols].dropna()
+cols= ["Survived", "Pclass", "Sex", "Age", "SibSp", "Parch", "Fare", "Embarked"] #selecting columns
+titanic= titanic[cols].dropna() #reducing into chosen columns and dropping rows that don't have values 
+
+###Seaborn!!!!!
+
+# histogram
+sns.distplot(titanic["Age"])
+
+# kernel density
+sns.kdeplot(titanic["Age"], shade= True)
+plt.xlabel("Age")
