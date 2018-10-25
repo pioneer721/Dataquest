5d9ab89e351c1ef5e93c2ac219191b4ac6725944
pwjeong14 <pilwon95@gmail.com>
2018-10-25 23:29:58 +0900

    Create Summary.py

---
+++ Summary.py
@@ -0,0 +1,13 @@
+                                          Data Analysis and Visualization 
+
+                                           Pandas and NumPy Fundamentals 
+
+"""
+Data Columns:
+Manufacturer	Model Name	Category	Screen Size	Screen	CPU	RAM	Storage	GPU	
+Operating System	Operating System Version	Weight	Price (Euros)
+"""
+
+laptops= pd.read_csv("laptops.csv", encoding= "Latin-1") 
+
+
